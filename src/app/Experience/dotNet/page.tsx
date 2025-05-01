791f2bc6b3e2788e27af92379e6cb30eabd2092d
YoshiakiKurihara <yoshiakikurihara@gmail.com>
2025-05-01 14:00:02 +1000

    Fix: Edit Routeing to Dispaly Profile on Main Page.

--- src/app/Experience/dotNet/page.tsx
+++ src/app/Experience/dotNet/page.tsx
@@ -4,6 +4,6 @@ import { MessageText } from '../../components/messages/Messages'
 export default async function Experience_dotNet() {
 
   return (
-    getBlogPosts(MessageText.TITLE_EXPERIENCE_DOTNET, process.env.NOTION_EXP_dotNET_DATABASE_ID as string)
+    getBlogPosts(MessageText.TITLE_EXPERIENCE_DOTNET, process.env.NOTION_EXP_DOTNET_DATABASE_ID as string)
   )
 }
--- src/app/Minutes/dotNet/page.tsx
+++ src/app/Minutes/dotNet/page.tsx
@@ -4,6 +4,6 @@ import { MessageText } from '../../components/messages/Messages'
 export default async function Experience_dotNet() {
 
   return (
-    getBlogPosts(MessageText.TITLE_DISCUSSION_DOTNET, process.env.NOTION_DISC_dotNET_DATABASE_ID as string)
+    getBlogPosts(MessageText.TITLE_DISCUSSION_DOTNET, process.env.NOTION_DISC_DOTNET_DATABASE_ID as string)
   )
 }
